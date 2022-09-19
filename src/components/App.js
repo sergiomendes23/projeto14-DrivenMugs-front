a12203ffffdbb431f4d1725dc17109e1d03359fd
ThalessAp <thalesalves7839@gmail.com>
2022-09-19 12:13:49 -0300

    update my

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,17 @@ import Home from "./Account/Home";
 import Login from "./Login/Login";
 import SignUp from "./SignUp/SignUp";
 
-export default function App () {
-
-    return (
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/cart" element={<Cart />} />
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
-    )
-};
+export default function App() {
+	return (
+		<UserProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/cart" element={<Cart />} />
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
+	);
+}
